06ce1656035c3d0d4cb9ad0d8216b9c5f96efcc1
OdilovMM <dssrinman@gmail.com>
2024-08-04 19:50:57 +0500

    Add: addReview method reviewController

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -1,9 +1,30 @@
 const { StatusCodes } = require("http-status-codes");
-const { CustomAPIError } = require("../errors");
+const { CustomAPIError, NotFoundError, BadRequestError } = require("../errors");
 const Review = require("../models/reviewModel");
+const Product = require("../models/productModel");
 
-const addReview = (req, res) => {
-  res.send("Review Controller");
+const addReview = async (req, res) => {
+  const { product: productId } = req.body;
+  const isExistProduct = await Product.findOne({ _id: productId });
+
+  if (!isExistProduct) {
+    throw new NotFoundError("No product found with that id");
+  }
+  const isExistReview = await Review.findOne({
+    product: productId,
+    user: req.user.userId,
+  });
+
+  if (isExistReview) {
+    throw new BadRequestError(
+      "You have already added a review, A user can only add one review for a product"
+    );
+  }
+
+  req.body.user = req.user.userId;
+  const review = await Review.create(req.body);
+
+  res.status(StatusCodes.CREATED).json({ msg: "A Review added", review });
 };
 
 const getAllReviews = (req, res) => {
